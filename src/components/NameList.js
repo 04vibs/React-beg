9852a46e195bc47e7866a5a41c7fe8be066924c6
04vibs <04vibs@gmail.com>
2022-01-21 19:07:51 +0530

    list rendering

---
+++ src/components/NameList.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import Person from './Person';
+
+function NameList() {
+    
+    const persons = [
+        {
+            id: 1,
+            name: 'Prashant',
+            age: 30,
+            skill: 'React'
+        },
+        {
+            id: 2,
+            name: 'Rahul',
+            age: 31,
+            skill: 'Angular'
+        }
+    ]
+    const names = ['Prashant','Rahul','Rohan'];
+    const nameList = names.map(name => <h2>{name}</h2>)
+    //using props for person List
+    const personList = persons.map(person => <Person person={person}/>)
+    return (
+    <div>
+        {nameList}
+        {personList}
+    </div>
+  );
+}
+
+export default NameList;
